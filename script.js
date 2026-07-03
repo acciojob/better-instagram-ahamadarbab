21cedc9eeb5ac615ad9c99556370b0ca31a87fcf
Arbab Ahamad <ahmadarbab.456@gmail.com>
2026-07-03 17:23:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your code here
+const images = document.querySelectorAll(".image");
+
+let draggedItem = null;
+
+images.forEach((image) => {
+	image.addEventListener("dragstart", () => {
+		draggedItem = this;
+		this.classList.add("selected");
+	});
+
+	image.addEventListener("dragend", () => {
+		this.classList.remove("selected");
+	});
+
+	image.addEventListener("dragover", (e) => {
+		e.preventDefault();
+	});
+
+	image.addEventListener("drop", (e) => {
+		e.preventDefault();
+
+		if(draggedItem === this) return;
+
+		// Swap IDs so background images are swapped
+		const tempId = draggedItem.id;
+		draggedItem.id = this.id;
+		this.id = tempId;
+	});
+});
